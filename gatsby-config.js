ee3f451c607f68d73bcc676ef4a8f4d188b8f169
Steve <9054646+stendur@users.noreply.github.com>
2021-06-06 14:31:12 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,22 @@ module.exports = {
   siteMetadata: {
     title: "fish-food",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: ["gatsby-plugin-sass", `gatsby-plugin-image`,
+  `gatsby-plugin-sharp`, `gatsby-transformer-sharp`],
 };
+
+const path = require(`path`)
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+}
